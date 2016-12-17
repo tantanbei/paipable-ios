d6cd6f40ab9c9bd24a711c3b309270f2daab6cc3
tantanbei <tan1115@outlook.com>
2016-12-17 14:31:24 +0800

    MOD add the init project

---
+++ paipable/paipable/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  paipable
+//
+//  Created by 谈凯德 on 17/12/2016.
+//  Copyright © 2016 谈凯德. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
